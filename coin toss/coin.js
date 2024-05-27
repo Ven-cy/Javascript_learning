1231bb15f9efee801be1205e88218f6dc4a41a3e
Ven-cy <160127240+Ven-cy@users.noreply.github.com>
2024-05-27 17:30:41 +0530

    correct code

--- coin toss/coin.js
+++ coin toss/coin.js
@@ -9,9 +9,7 @@ tossBtn.addEventListener('click', () => {
 function tossCoinFunction() { 
 	const randomVal = Math.random(); 
 	const faceCoin = randomVal < 0.5 ? 'Heads' : 'Tails'; 
-	const imageUrl = faceCoin === 'Heads' ? 
-'https://iili.io/JLUnkT7.png' : 
-'https://iili.io/JLUzIh7.png'; 
+	const imageUrl = faceCoin === 'Heads' ?'https://iili.io/JLUnkT7.png':'https://iili.io/JLUzIh7.png'; 
 		
 	coinIcon.classList.add('flip'); 
 	setTimeout(() => { 
